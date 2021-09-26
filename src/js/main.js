0cd658536f9f0ce3f4569fa04f7d8f1d6c066714
Vanik Egoyan <you@example.com>
2021-09-26 12:50:09 +0300

    add playvideo

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 import Slider from './modules/slider';
+import VideoPlayer from './modules/playVideo';
 
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');//экземпляр класса
     slider.render();
+
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
+
 });
