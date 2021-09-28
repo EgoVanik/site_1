d070eb01c7a71ea05f484a9c2a40b0e501407009
Vanik Egoyan <you@example.com>
2021-09-28 21:09:08 +0300

    add moduleshtml and mod slider

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -43,11 +43,7 @@ export default class MainSlider extends Slider {
         this.showSlides(this.slideIndex += n);
     }
 
-    render() {
-        try {
-            this.hanson = document.querySelector('.hanson');
-        } catch(e){}
-
+    bindTriggers() {
         this.btns.forEach(btn => {
             btn.addEventListener('click', () => {
                 this.plusSlides(1);
@@ -60,6 +56,33 @@ export default class MainSlider extends Slider {
             })
         });
 
-        this.showSlides(this.slideIndex);
+        document.querySelectorAll('.prevmodule').forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(-1);
+            });
+        });
+
+        document.querySelectorAll('.nextmodule').forEach(item => {
+            item.addEventListener('click', (e) => {
+                // отменяем всплытие лишних событий(если класс повторяется и код выполняется для других стрелок)
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(1);
+            });
+        });
+    }
+
+    render() {
+        // т.к. контейнеры могуть быть разными, првоеряем есть ли на странице контейнер
+        if (this.container) {
+            try {
+                this.hanson = document.querySelector('.hanson');
+            } catch(e){}
+
+            this.showSlides(this.slideIndex);
+            this.bindTriggers();
+        }
     }
 }
